8b51396832d3cdf8e257a2c0c89ebafa406fc3e1
s1212w <s1212w@naver.com>
2023-04-06 17:37:06 +0900

    ADD : 4/6 미팅, sns 페이지 기능 구현
    1. meeting 패키지 추가
    2. sns 패키지 추가

---
+++ src/main/java/com/kh/spouting/meeting/domain/Meeting.java
@@ -0,0 +1,14 @@
+package com.kh.spouting.meeting.domain;
+
+import java.sql.Timestamp;
+
+public class Meeting {
+	
+	private int meetingNo;
+	private int readerNo;
+	private String meetingName;
+	private String meetingDetail;
+	private int meetingPeople;
+	private Timestamp meetingDay;
+
+}
---
+++ src/main/java/com/kh/spouting/sns/domain/Sns.java
@@ -0,0 +1,5 @@
+package com.kh.spouting.sns.domain;
+
+public class Sns {
+
+}
